b8a9607057f970c0296211ae0b4c49d0b39724ba
Tenaity <tenaiti.150320@gmail.com>
2021-04-22 11:49:27 +0700

    Done SignUp SignIn with Firebase Realtime

---
+++ app/src/main/java/com/midterm/plantsfirebase1/MainActivity.java
@@ -0,0 +1,35 @@
+package com.midterm.plantsfirebase1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnSignup, btnSignin;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnSignin = findViewById(R.id.btn_signin);
+        btnSignup = findViewById(R.id.btn_sigup);
+
+        btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent signup = new Intent(MainActivity.this,SignUp.class);
+                startActivity(signup);
+            }
+        });
+        btnSignin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent signIn = new Intent(MainActivity.this,SignIn.class);
+                startActivity(signIn);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/midterm/plantsfirebase1/SignUp.java
@@ -0,0 +1,65 @@
+package com.midterm.plantsfirebase1;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.midterm.plantsfirebase1.Model.User;
+
+public class SignUp extends AppCompatActivity {
+
+    EditText edtPhone,edtName,edtPassword;
+    Button btnSignUp;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        edtPhone = findViewById(R.id.edt_phoneSignup);
+        edtName = findViewById(R.id.edt_nameSignup);
+        edtPassword = findViewById(R.id.edt_passwordSignup);
+
+        btnSignUp = findViewById(R.id.btn_signUp);
+
+        //Init Firebase
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+        final DatabaseReference table_user = database.getReference("User");
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                table_user.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        if(snapshot.child(edtPhone.getText().toString()).exists()){
+                            Toast.makeText(SignUp.this,"Phone number already register!",Toast.LENGTH_SHORT).show();
+                        }
+                        else{
+                            User user = new User(edtName.getText().toString(),edtPassword.getText().toString());
+                            table_user.child(edtPhone.getText().toString()).setValue(user);
+                            Toast.makeText(SignUp.this,"Sign up Successfully!",Toast.LENGTH_SHORT).show();
+                            finish();
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+            }
+        });
+
+    }
+}
